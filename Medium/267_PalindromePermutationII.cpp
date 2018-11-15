5e290be83090013f811f3a77ffcead6f15bc50f1
Chenyu Peng <j-pcy@Chenyus-MacBook-Pro.local>
2018-11-15 15:29:18 -0800

    11/15/2018

--- Medium/267_PalindromePermutationII.cpp
+++ Medium/267_PalindromePermutationII.cpp
@@ -13,6 +13,7 @@ Output: []
 
 class Solution {
 public:
+/*
     vector<string> generatePalindromes(string s) {
         unordered_set<string> res;
         unordered_map<char, int> m;
@@ -36,6 +37,33 @@ private:
             swap(t[start], t[i]);
             permute(t, start + 1, mid, res);
             swap(t[start], t[i]);
+        }
+    }*/
+    vector<string> generatePalindromes(string s) {
+        vector<string> res;
+        unordered_map<char, int> m;
+        string t = "", mid = "";
+        for (auto a : s) ++m[a];
+        for (auto &it : m) {
+            if (it.second % 2 == 1) mid += it.first;
+            it.second /= 2;
+            t += string(it.second, it.first);
+            if (mid.size() > 1) return {};
+        }
+        permute(t, m, mid, "", res);
+        return res;
+    }
+    void permute(string &t, unordered_map<char, int> &m, string mid, string out, vector<string> &res) {
+        if (out.size() >= t.size()) {
+            res.push_back(out + mid + string(out.rbegin(), out.rend()));
+            return;
+        } 
+        for (auto &it : m) {
+            if (it.second > 0) {
+                --it.second;
+                permute(t, m, mid, out + it.first, res);
+                ++it.second;
+            }
         }
     }
 };
